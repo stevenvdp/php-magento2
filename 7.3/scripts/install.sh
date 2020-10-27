b627dc8277137e37d99fbb355f623f8aebf981d3
Steven Vandeputte <steven.vandeputte@studioemma.eu>
2020-10-27 08:34:24 +0100

    Downgrade composer version (1.10.16)
    Base image installs the latest version of composer, at the moment Magento 2 is not compatible with composer v2
    See: https://github.com/magento/community-features/issues/302

--- 7.3/scripts/install.sh
+++ 7.3/scripts/install.sh
@@ -20,5 +20,15 @@ sed -e 's/\(max_nesting_level\).*/\1=1000/' \
 curl -sS -o /usr/local/bin/magerun https://files.magerun.net/n98-magerun2.phar
 chmod +x /usr/local/bin/magerun
 
+# install composer v1
+if [[ -e /usr/local/bin/composer ]]; then
+    rm /usr/local/bin/composer
+fi
+
+curl -LsS https://getcomposer.org/installer | \
+    php -- --version=1.10.16 --install-dir=/usr/local/lib --filename=composer
+chmod +x /usr/local/lib/composer
+ln -s /usr/local/lib/composer /usr/local/bin/composer
+
 # update permissions to allow rootless operation
 /usr/local/bin/permissions
